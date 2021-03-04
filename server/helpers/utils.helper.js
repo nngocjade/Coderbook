1050296abbfc353244652c27c1f26a8d21ec3a07
nngocjade <nngocjade@gmail.com>
2021-03-04 15:28:34 +0700

    refactored response error message

--- server/helpers/utils.helper.js
+++ server/helpers/utils.helper.js
@@ -6,10 +6,11 @@ const utilsHelper = {};
 // If we need to change the way to response later on, we only need to handle it here
 utilsHelper.sendResponse = (res, status, success, data, errors, message) => {
   const response = {};
-  if (success) response.success = success;
+  response.success = success;
   if (data) response.data = data;
   if (errors) response.errors = errors;
   if (message) response.message = message;
+  response.status = status;
   return res.status(status).json(response);
 };
 
